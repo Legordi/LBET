d6a31a193e4dbb8ce0c2aab732b645a5aa00d30c
Legordi <legordi@gmail.com>
2025-06-12 03:56:24 -0500

    Limpio conflictos de merge y dejo solo mi versión

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/Home.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-=======
 import React from "react"; 
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -22,6 +12,5 @@ createRoot(document.getElementById("root")).render(
         <App />
       </BrowserRouter>
     </AuthProvider>
->>>>>>> 1d301f66074100048eca34a09004effca1aa4b73
   </React.StrictMode>
 );
